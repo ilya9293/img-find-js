0eed876b62f354e0ea6dff319c6b117b7da68650
ilya9293 <ilya9293@ukr.net>
2022-04-21 21:02:25 +0300

    create folders

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import './sass/main.scss';
+import refs from './js/data/refs';
+import fetchImages from './js/services/apiService';
 
+const onSubmit = e => {
+  e.preventDefault();
+
+  const BASE_URL = ' https://pixabay.com/api/';
+  const urlParams = new URLSearchParams({
+    image_type: 'photo',
+    orientation: 'horizontal',
+    q: e.currentTarget.query.value,
+    page: 1,
+    per_page: 12,
+    key: '26909021-bb302c7a297d7b4d207aa52f9',
+  });
+
+  fetchImages(`${BASE_URL}?${urlParams}`).then(data => console.log(data));
+};
+
+refs.form.addEventListener('submit', onSubmit);
